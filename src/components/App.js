1ab82cc9d51dc48f0fec8fc9b6cc13e5925823f9
Yuriy Vasilyev <yuriy.vassilyev@gmail.com>
2017-09-10 10:56:53 +0300

    Add dependencies, create basic file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="readable-app">
-
+      <div className='container'>
+        Hello, world!
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware( thunk )
+  )
+);
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
